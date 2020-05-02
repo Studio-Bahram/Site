e5c72dcb755a05e42f392a532e89a78e4cc16c8f
i007c <dr007cc@gmail.com>
2020-05-03 01:12:08 +0430

    make file Studiobahram_ir/origin/urls.py and write to this #Comment

---
+++ Studiobahram_ir/origin/urls.py
@@ -0,0 +1,20 @@
+
+
+
+
+# from django.urls import path
+# from django.conf import settings
+# from django.conf.urls.static import static
+
+# from . import views
+
+# urlpatterns = [
+#     path('', views.index, name='index'),
+#     path('index.html', views.index, name='index'),
+#     path('bots.html', views.bots, name='bots'),
+#     path('about.html', views.about, name='about'),
+#     path('index-fa.html', views.fa_index, name='fa_index'),
+#     path('bots-fa.html', views.fa_bots, name='fa_bots'),
+#     path('about-fa.html', views.fa_about, name='fa_about'),
+#     path('posts/', views.posts, name='posts'),
+# ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
