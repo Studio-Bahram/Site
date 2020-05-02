cdcb7573a88a336c4e61494cbb6a585d4cf644bf
i007c <dr007cc@gmail.com>
2020-05-03 01:10:56 +0430

    add INSTALLED_APPS["origin.apps.OriginConfig"] & line 37 add MEDIA_ROOT,MEDIA_URL to file settings.py

--- Studiobahram_ir/Studiobahram_ir/settings.py
+++ Studiobahram_ir/Studiobahram_ir/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'origin.apps.OriginConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, '/home/aria/Desktop/Site/StudioBahram_ir/db_files/')
+MEDIA_URL = '/media/'
