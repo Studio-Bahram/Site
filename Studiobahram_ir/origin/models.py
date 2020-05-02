dccbd81e26cd483f7845b8fef0aaba47dcb41fee
i007c <dr007cc@gmail.com>
2020-05-03 03:14:11 +0430

    clean in file models.py

--- Studiobahram_ir/origin/models.py
+++ Studiobahram_ir/origin/models.py
@@ -7,8 +7,8 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=400)
     pub_date = models.DateField()
-    picture = models.ImageField(upload_to="db_files/posts/pics/%Y/%m/%d/",default="db_files/posts/pics/%Y/%m/%d/img.jpg")
-    file_post = models.FileField(upload_to="db_files/posts/files/%Y/%m/%d/",default="db_files/posts/files/%Y/%m/%d/file")
+    picture = models.ImageField(upload_to="posts/pics/%Y/%m/%d/",default="posts/pics/%Y/%m/%d/img.jpg")
+    file_post = models.FileField(upload_to="posts/files/%Y/%m/%d/",default="posts/files/%Y/%m/%d/file")
     about_post = models.TextField(help_text="Writeing about post ...")
     github_url = models.URLField(max_length=300,default="https://github.com/")
     
@@ -18,10 +18,9 @@ class Post(models.Model):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Instruction(models.Model):
-    title = models.CharField(max_length=400,name="Instruction TITLE")
-    slug = models.SlugField(unique=True)
+    title = models.CharField(max_length=400,name="Instruction Title")
     pub_date = models.DateField()
-    instruction_text = models.TextField(help_text="Writeing text Instruction ...")
+    instruction_text = models.TextField(help_text="Writeing text Instruction ...",name="Instruction Text")
     youtube_url = models.URLField(name="YOUTUBE URL")
     
     def __str__(self):
